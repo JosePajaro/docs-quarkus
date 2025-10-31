f1045d618a78f1ff3ec5cffb141d75b55942cb06
Jose Luis Pajaro0 <jose.pajaro@naut.com.co>
2025-10-31 15:41:57 -0500

    feat: deleting unnecessary file

--- src/main/java/org/acme/ProtectedResource.java
+++
@@ -1,9 +0,0 @@
-package org.acme;
-
-import jakarta.inject.Inject;
-import org.keycloak.authorization.client.AuthzClient;
-
-public class ProtectedResource {
-    @Inject
-    AuthzClient authzClient;
-}
